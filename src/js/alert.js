6b32efcf95e36587a47c5272851d9302192c550b
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-05-03 21:54:39 +0530

    prject initiated

---
+++ src/js/alert.js
@@ -0,0 +1,9 @@
+// Alert a message on the browser window
+// Really Exciting
+//
+//
+alert('I am learning Gulp JS');
+
+//
+//
+console.log('Log Nothing');
---
+++ src/js/log.js
@@ -0,0 +1,14 @@
+// Alert a message on the browser window
+// Really Exciting
+console.log('Log Something on the Console');
+//
+//
+
+console.log('Log Something Else');
+
+//
+//
+
+//
+
+console.log('Log Nothing');
