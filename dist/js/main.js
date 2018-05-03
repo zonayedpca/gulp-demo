d30a6cf494f7d1a7c6b0683f126473725448cf90
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-05-03 22:02:14 +0530

    Gulp Final Project

---
+++ dist/js/main.js
@@ -0,0 +1,2 @@
+alert("I am learning Gulp JS"),console.log("Log Nothing");
+console.log("Log Something on the Console"),console.log("Log Something Else"),console.log("Log Nothing");
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,45 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 gulp.task('hello', function() {
   console.log('Hi! This is my First Task!!');
 });
+
+gulp.task('copy', function() {
+   gulp.src('./src/*.html').pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./src/css/*.scss')
+    .pipe(sass())
+      .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('compress', function() {
+  gulp.src('./src/js/*')
+    .pipe(uglify())
+      .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('concat', function() {
+  gulp.src('./src/js/*')
+    .pipe(concat('main.js'))
+      .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('concatmin', function() {
+  gulp.src('./src/js/*')
+    .pipe(uglify())
+      .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/*.html', ['copy']);
+  gulp.watch('./src/css/*.scss', ['sass']);
+  gulp.watch('./src/js/*', ['concatmin']);
+});
+
+gulp.task('default', ['copy', 'sass', 'concatmin', 'watch']);
